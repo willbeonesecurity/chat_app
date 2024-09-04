9230c3d3db9bfc8c652343d0c3a00bfbe6fb9cbf
Christopher Hart <christopher@willbeonesecurity.xyz>
2024-09-04 14:24:02 +0200

    fix google social login
    	modified:   app/api/auth/[...nextauth]/route.ts
    	modified:   package-lock.json

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -16,8 +16,13 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+      authorization: {
+        params: {
+          scope: 'openid profile email',
+        }
+      }
     }),
     CredentialsProvider({
       name: 'credentials',
