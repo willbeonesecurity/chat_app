b9737b8527ed0994470ccec3892d8c58130e057a
Christopher Hart <christopher@willbeonesecurity.xyz>
2024-09-10 10:31:06 +0200

    finished navigation sidebar and avatar

--- app/(site)/components/sidebar/Sidebar.tsx
+++ app/(site)/components/sidebar/Sidebar.tsx
@@ -1,17 +1,19 @@
 import DesktopSidebar from "./DesktopSidebar";
 import MobileFooter from "./MobileFooter";
+import getCurrentUser from "@/app/actions/getcurrentUser";
 
-async function Sidebar ({ children}: {
+async function Sidebar({ children }: {
     children: React.ReactNode;
 }) {
+    const currentUser = await getCurrentUser();
     return (
         <div className="h-full">
-            <DesktopSidebar />
+            <DesktopSidebar currentUser={currentUser!} />
             <MobileFooter />
             <main className="lg:pl-20 h-full">
                 {children}
             </main>
-            </div>
+        </div>
     )
 }
 
